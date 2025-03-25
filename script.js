66d61dbfb569e4ae7696b69cfcb74f0a1e8d0a48
Gabriel Mirand <gabrielfomiranda@gmail.com>
2025-03-25 16:43:06 -0300

    rename offsetLimit to offsetTarget

--- script.js
+++ script.js
@@ -16,21 +16,21 @@ const SPEED = 1.5;
 const slot1 = {
   items: document.querySelectorAll(".slot-1 > .item"),
   offset: 0,
-  offsetLimit: 0,
+  offsetTarget: 0,
   lastUpdate: Date.now(),
 };
 
 const slot2 = {
   items: document.querySelectorAll(".slot-2 > .item"),
   offset: 0,
-  offsetLimit: 0,
+  offsetTarget: 0,
   lastUpdate: Date.now(),
 };
 
 const slot3 = {
   items: document.querySelectorAll(".slot-3 > .item"),
   offset: 0,
-  offsetLimit: 0,
+  offsetTarget: 0,
   lastUpdate: Date.now(),
 };
 
@@ -57,12 +57,12 @@ const spinSlot = (slot, result, turn, cb) => {
   const update = () => {
     const currentTime = Date.now();
 
-    if (slot.offset < slot.offsetLimit) {
+    if (slot.offset < slot.offsetTarget) {
       const ellapsedTime = currentTime - slot.lastUpdate;
       slot.offset += SPEED * ellapsedTime;
       window.requestAnimationFrame(update);
     } else {
-      slot.offset = slot.offsetLimit % TOTAL_HEIGHT;
+      slot.offset = slot.offsetTarget % TOTAL_HEIGHT;
       if (cb) {
         cb();
       }
@@ -86,7 +86,7 @@ const spinSlot = (slot, result, turn, cb) => {
   };
 
   slot.lastUpdate = Date.now();
-  slot.offsetLimit =
+  slot.offsetTarget =
     slot.offset +
     turn * TOTAL_HEIGHT +
     (TOTAL_HEIGHT + result * ITEM_HEIGHT) -
