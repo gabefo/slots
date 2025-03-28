4da4f492c4aef3a22891566a8500ae39fae66bdd
Gabriel Mirand <gabrielfomiranda@gmail.com>
2025-03-28 12:53:58 -0300

    fix animation

--- main.js
+++ main.js
@@ -12,25 +12,16 @@ const ITEMS = [
 const slot1 = {
   items: document.querySelectorAll(".slot-1 > .item"),
   offset: 0,
-  offsetTarget: 0,
-  speed: 0,
-  lastUpdate: Date.now(),
 };
 
 const slot2 = {
   items: document.querySelectorAll(".slot-2 > .item"),
   offset: 0,
-  offsetTarget: 0,
-  speed: 0,
-  lastUpdate: Date.now(),
 };
 
 const slot3 = {
   items: document.querySelectorAll(".slot-3 > .item"),
   offset: 0,
-  offsetTarget: 0,
-  speed: 0,
-  lastUpdate: Date.now(),
 };
 
 const playBtn = document.getElementById("play-btn");
@@ -50,9 +41,9 @@ const play = () => {
   playBtn.setAttribute("disabled", "");
   resultEl.innerHTML = "";
 
-  spinSlot(slot1, results[0], 500);
-  spinSlot(slot2, results[1], 1000);
-  spinSlot(slot3, results[2], 1500, () => {
+  spinSlot(slot1, 0, results[0]);
+  spinSlot(slot2, 1, results[1]);
+  spinSlot(slot3, 2, results[2], () => {
     playBtn.removeAttribute("disabled");
     resultEl.innerHTML = results.map((i) => ITEMS[i]).join(", ");
   });
@@ -66,36 +57,54 @@ const updatePosition = (slot) => {
   }
 };
 
-const spinSlot = (slot, result, duration, cb) => {
-  const update = () => {
-    const currentTime = Date.now();
+const spinSlot = (slot, slotIndex, result, cb) => {
+  const target = (5 + slotIndex) * slot.items.length + result;
+  const delay = 100 * slotIndex;
+  const duration = ((target - slot.offset) / slot.items.length) * 200;
 
-    if (slot.offset < slot.offsetTarget) {
-      const ellapsedTime = currentTime - slot.lastUpdate;
-      slot.offset += slot.speed * ellapsedTime;
-      window.requestAnimationFrame(update);
-    } else {
-      slot.offset = slot.offsetTarget;
+  console.log(duration);
+
+  anime({
+    targets: slot,
+    offset:
+      slotIndex < 2
+        ? [
+            {
+              value: target - 1,
+              duration: duration,
+              delay: delay,
+              easing: "linear",
+            },
+            {
+              value: target,
+              duration: 200,
+              easing: "easeOutElastic",
+            },
+          ]
+        : [
+            {
+              value: target,
+              duration: duration,
+              delay: delay,
+              easing: "linear",
+            },
+            {
+              value: target + slot.items.length,
+              duration: 1000,
+              easing: "easeOutQuad",
+            },
+          ],
+    update: () => {
+      updatePosition(slot);
+    },
+    complete: () => {
+      slot.offset = slot.offset % slot.items.length;
+
       if (cb) {
         cb();
       }
-    }
-
-    updatePosition(slot);
-
-    slot.lastUpdate = currentTime;
-  };
-
-  const offset = slot.offset % slot.items.length;
-  const offsetTarget =
-    (1 + Math.round(duration / 1000) * 3) * slot.items.length + result;
-  const speed = (offsetTarget - offset) / duration;
-
-  slot.offset = offset;
-  slot.offsetTarget = offsetTarget;
-  slot.speed = speed;
-  slot.lastUpdate = Date.now();
-  window.requestAnimationFrame(update);
+    },
+  });
 };
 
 init();
